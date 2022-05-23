7075f480ca7a0c836f171576569e632d37a58ae7
kd <kd>
2022-05-23 15:13:30 -0400

    build_06232022_8

--- src/components/SearchBoost.js
+++ src/components/SearchBoost.js
@@ -4,6 +4,8 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
+import Avatar from '@material-ui/core/Avatar';
+import Box from '@mui/material/Box';
 
 export default class Tags extends React.Component {
     state={
@@ -39,12 +41,26 @@ export default class Tags extends React.Component {
                     getOptionLabel={(option) => option.title}
                     filterSelectedOptions
                     onChange={this.handleChange}
+                    groupBy={(option) => option.group}
+                    renderOption={(props, option) => (
+                        <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+                          <img
+                            loading="lazy"
+                            width="20"
+                            src={option.img_src}
+                            srcSet={option.img_src}
+                            alt=""
+                          />
+                          {option.title}
+                        </Box>
+                      )}
                     renderInput={(params) => (
                         <TextField
                             {...params}
+                            
                             label="select boosts"
-                            placeholder="Favorites"
-                        />
+                            placeholder="Boosts"
+                        ></TextField>
                     )}
                 />
                 </Stack>
@@ -63,36 +79,37 @@ export default class Tags extends React.Component {
 }
 
 // Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
-const top100Films = [{'title':'Football Stadium'},
-{'title':'Baseball Stadium'},
-{'title':'Soccer Stadium'},
-{'title':'Wind Farm'},
-{'title':'Solar Farm'},
-{'title':'Nuclear Power Plant'},
-{'title':'Rocket Launch Site'},
-{'title':'Crypto Mining Facility'},
-{'title':'UnbelieVALTable'},
-{'title':'School'},
-{'title':'University'},
-{'title':'Town Hall'},
-{'title':'Cylinder Mansion'},
-{'title':'Organic Mansion'},
-{'title':'Winter Mega-Mansion'},
-{'title':'METPOT CASINO'},
-{'title':'Wildlife Waystation'},
-{'title':'Circ-o-verse'},
-{'title':'Cemetery'},
-{'title':'ETHRASH'},
-{'title':'Sewage Treatment'},
-{'title':'Hospital'},
-{'title':'Police Station'},
-{'title':'Fire Station'},
-{'title':'Bus Stop'},
-{'title':'Subway Station'},
-{'title':'Metaxi Station'},
-{'title':'Metroverse Theatre'},
-{'title':'Metroverse Museum'},
-{'title':'Metcinema'},
-{'title':'Double Rail'},
-{'title':'Double River'},
+const top100Films = [
+    {'title':'Football Stadium', 'img_src':'../media/icon_sports_safety_active.437d05c3.png', 'group':'Sports'},
+    {'title':'Baseball Stadium', 'img_src':'../media/icon_sports_safety_active.437d05c3.png', 'group':'Sports'},
+    {'title':'Soccer Stadium', 'img_src':'../media/icon_sports_safety_active.437d05c3.png', 'group':'Sports'},
+    {'title':'Wind Farm', 'img_src':'../media/icon_power_safety_active.fa2744a6.png', 'group':'Energy'},
+    {'title':'Solar Farm', 'img_src':'../media/icon_power_safety_active.fa2744a6.png', 'group':'Energy'},
+    {'title':'Nuclear Power Plant', 'img_src':'../media/icon_power_safety_active.fa2744a6.png', 'group':'Energy'},
+    {'title':'Rocket Launch Site', 'img_src':'../media/icon_technology_safety_active.95a48ef7.png', 'group':'Tech'},
+    {'title':'Crypto Mining Facility', 'img_src':'../media/icon_technology_safety_active.95a48ef7.png', 'group':'Tech'},
+    {'title':'UnbelieVALTable', 'img_src':'../media/icon_technology_safety_active.95a48ef7.png', 'group':'Tech'},
+    {'title':'School', 'img_src':'../media/icon_bonus_education_active.d9343487.png', 'group':'Education'},
+    {'title':'University', 'img_src':'../media/icon_bonus_education_active.d9343487.png', 'group':'Education'},
+    {'title':'Town Hall', 'img_src':'../media/icon_bonus_education_active.d9343487.png', 'group':'Education'},
+    {'title':'Cylinder Mansion', 'img_src':'../media/icon_mansion_safety_active.51f566df.png', 'group':'Mansion'},
+    {'title':'Organic Mansion', 'img_src':'../media/icon_mansion_safety_active.51f566df.png', 'group':'Mansion'},
+    {'title':'Winter Mega-Mansion', 'img_src':'../media/icon_mansion_safety_active.51f566df.png', 'group':'Mansion'},
+    {'title':'METPOT CASINO', 'img_src':'../media/icon_entertainment_safety_active.aafb04cd.png', 'group':'Entertainment'},
+    {'title':'Wildlife Waystation', 'img_src':'../media/icon_entertainment_safety_active.aafb04cd.png', 'group':'Entertainment'},
+    {'title':'Circ-o-verse', 'img_src':'../media/icon_entertainment_safety_active.aafb04cd.png', 'group':'Entertainment'},
+    {'title':'Cemetery', 'img_src':'../media/icon_bonus_odds_active.838ed4c7.png', 'group':'Odds'},
+    {'title':'ETHRASH', 'img_src':'../media/icon_bonus_odds_active.838ed4c7.png', 'group':'Odds'},
+    {'title':'Sewage Treatment', 'img_src':'../media/icon_bonus_odds_active.838ed4c7.png', 'group':'Odds'},
+    {'title':'Hospital', 'img_src':'../media/icon_bonus_safety_active.8080e503.png', 'group':'Safety'},
+    {'title':'Police Station', 'img_src':'../media/icon_bonus_safety_active.8080e503.png', 'group':'Safety'},
+    {'title':'Fire Station', 'img_src':'../media/icon_bonus_safety_active.8080e503.png', 'group':'Safety'},
+    {'title':'Bus Stop', 'img_src':'../media/icon_bonus_transparent_active.dafc84a8.png', 'group':'Transport'},
+    {'title':'Subway Station', 'img_src':'../media/icon_bonus_transparent_active.dafc84a8.png', 'group':'Transport'},
+    {'title':'Metaxi Station', 'img_src':'../media/icon_bonus_transparent_active.dafc84a8.png', 'group':'Transport'},
+    {'title':'Metroverse Theatre', 'img_src':'../media/icon_cultural_safety_active.723a9102.png', 'group':'Cultural'},
+    {'title':'Metroverse Museum', 'img_src':'../media/icon_cultural_safety_active.723a9102.png', 'group':'Cultural'},
+    {'title':'Metcinema', 'img_src':'../media/icon_cultural_safety_active.723a9102.png', 'group':'Cultural'},
+    {'title':'Double Rail', 'img_src':'../media/icon_bonus_river_active.45e4764a.png', 'group':'Railway'},
+    {'title':'Double River', 'img_src':'../media/icon_bonus_river_active.45e4764a.png', 'group':'River'},
 ]
